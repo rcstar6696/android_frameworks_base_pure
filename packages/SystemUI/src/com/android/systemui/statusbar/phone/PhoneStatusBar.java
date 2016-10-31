27d84a59126968ebddb2e1fabd2d0432267f9844
maxwen <max.weninger@gmail.com>
2016-10-31 23:48:50 +0100

    base: SystemUI: make QS columns count configurable
    And add infrastructure to add more settings and
    distribute them to QS panel
    
    Based on DU's commit https://github.com/DirtyUnicorns/android_frameworks_base/commit/156da9cc5e186bc8860b3f043cee236693c4655a
    
    just without the reverts

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -572,7 +572,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
                     Settings.System.ACCELEROMETER_ROTATION),
                     false, this, UserHandle.USER_ALL);
-
+            mContext.getContentResolver().registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_LAYOUT_COLUMNS),
+                    false, this, UserHandle.USER_ALL);
             update();
         }
 
@@ -583,6 +585,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         public void update() {
             mStatusBarWindowManager.updateKeyguardScreenRotation();
+            if (mHeader != null) {
+                mHeader.updateSettings();
+            }
         }
     }
     private OmniSettingsObserver mOmniSettingsObserver = new OmniSettingsObserver(mHandler);
